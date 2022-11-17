97f547746a7ccd38de05b282c3db18943ec3618f
Mohamed Hamouda <107848993+mohamedhamoudadevop@users.noreply.github.com>
2022-11-17 21:24:49 +0200

    change app bar title

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:  Text('Islami')),
+      appBar: AppBar(title:  Text('islamiiii')),
     );
   }
 }
